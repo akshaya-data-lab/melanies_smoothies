f6db3c1e829081b4499dc6e1d9e1406a0d151ec3
akshaya-data-lab <akshaya.n@datamavericks.com>
2026-03-25 17:29:17 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
+import requests  
 
 # Write directly to the app.
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -25,9 +26,15 @@ ingredients_list = st.multiselect(
 if ingredients_list:
 
     ingredients_string=''
+  
 
     for fruit_chosen in ingredients_list:
-        ingredients_string+=fruit_chosen+' '
+      ingredients_string+=fruit_chosen+' '
+      smoothiefroot_reponse=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
+       
+  
 
     #st.write(ingredients_string)
 
@@ -45,7 +52,5 @@ if ingredients_list:
 
 
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
